cd96c7ed0cddeaf797117638209dbe8c9aec1098
Rohit Kumar <rk7739484@gmail.com>
2026-05-30 16:07:38 +0530

    fix server.js merge conflict

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 import dotenv from "dotenv";
-
 dotenv.config();
 
 import app from "./app.js";
@@ -9,13 +8,7 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-<<<<<<< HEAD
-if (process.env.NODE_ENV !== 'production' && process.env.VERCEL !== '1') {
-=======
-// Local dev: listen on port
-// Vercel serverless: export app
-if (process.env.NODE_ENV !== "production" || process.env.VERCEL !== "1") {
->>>>>>> a0774128efee1cf4deafdd131202796e18da0b0f
+if (process.env.VERCEL !== "1") {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
